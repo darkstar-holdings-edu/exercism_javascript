05d5caf2621a1c176f1b203b30f3c900c9a420f9
David Betz <hashref@gmail.com>
2023-03-20 15:16:40 -0700

    Modify for ChatGPT suggested refactor

--- roman-numerals/roman-numerals.js
+++ roman-numerals/roman-numerals.js
@@ -1,32 +1,30 @@
-const DECIMAL_TO_ROMAN_MAP = {
-  1000: 'M',
-  900: 'CM',
-  500: 'D',
-  400: 'CD',
-  100: 'C',
-  90: 'XC',
-  50: 'L',
-  40: 'XL',
-  10: 'X',
-  9: 'IX',
-  6: 'VI',
-  5: 'V',
-  4: 'IV',
-  1: 'I',
-};
+const DECIMAL_TO_ROMAN_MAP = [
+  [1000, 'M'],
+  [900, 'CM'],
+  [500, 'D'],
+  [400, 'CD'],
+  [100, 'C'],
+  [90, 'XC'],
+  [50, 'L'],
+  [40, 'XL'],
+  [10, 'X'],
+  [9, 'IX'],
+  [6, 'VI'],
+  [5, 'V'],
+  [4, 'IV'],
+  [1, 'I'],
+];
 
 export const toRoman = (decimal) => {
-  let currentDecimal = decimal;
   let romanConversion = '';
+  let currentValue = decimal;
 
-  Object.entries(DECIMAL_TO_ROMAN_MAP)
-    .reverse()
-    .forEach(([key, value]) => {
-      while (currentDecimal >= key) {
-        romanConversion += value;
-        currentDecimal -= key;
-      }
-    });
+  for (const [key, value] of DECIMAL_TO_ROMAN_MAP) {
+    while (currentValue >= key) {
+      romanConversion += value;
+      currentValue -= key;
+    }
+  }
 
   return romanConversion;
 };

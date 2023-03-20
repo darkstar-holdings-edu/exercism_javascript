e2f2ae392e17b776182dba3c8167ca3ac848d5a5
David Betz <hashref@gmail.com>
2023-03-20 15:12:09 -0700

    Add solution for roman numeral exercise.

--- roman-numerals/roman-numerals.js
+++ roman-numerals/roman-numerals.js
@@ -1,8 +1,32 @@
-//
-// This is only a SKELETON file for the 'Roman Numerals' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const DECIMAL_TO_ROMAN_MAP = {
+  1000: 'M',
+  900: 'CM',
+  500: 'D',
+  400: 'CD',
+  100: 'C',
+  90: 'XC',
+  50: 'L',
+  40: 'XL',
+  10: 'X',
+  9: 'IX',
+  6: 'VI',
+  5: 'V',
+  4: 'IV',
+  1: 'I',
+};
 
-export const toRoman = () => {
-  throw new Error('Remove this statement and implement this function');
+export const toRoman = (decimal) => {
+  let currentDecimal = decimal;
+  let romanConversion = '';
+
+  Object.entries(DECIMAL_TO_ROMAN_MAP)
+    .reverse()
+    .forEach(([key, value]) => {
+      while (currentDecimal >= key) {
+        romanConversion += value;
+        currentDecimal -= key;
+      }
+    });
+
+  return romanConversion;
 };

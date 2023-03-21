083dc19c878ca09ea9c280b8d32be14c1009cd04
David Betz <hashref@gmail.com>
2023-03-20 19:02:29 -0700

    Add solution for perfect-numbers exercise

--- perfect-numbers/perfect-numbers.js
+++ perfect-numbers/perfect-numbers.js
@@ -1,8 +1,34 @@
-//
-// This is only a SKELETON file for the 'Perfect Numbers' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const { floor, sqrt } = Math;
 
-export const classify = () => {
-  throw new Error('Remove this statement and implement this function');
+const factors = (n) => [...Array(n).keys()].filter((i) => n % i === 0);
+
+const isPrime = (n) => {
+  if (n === 0 || n === 1) return false;
+  if (n === 2) return true;
+
+  for (let i = 2; i <= floor(sqrt(n)) + 1; i++) {
+    if (n % i === 0) return false;
+  }
+
+  return true;
+};
+
+export const classify = (n) => {
+  if (n < 1) throw new Error('Classification is only possible for natural numbers.');
+  if (n === 1 || isPrime(n)) return 'deficient';
+
+  const sum = factors(n).reduce((acc, cur) => acc + cur);
+
+  switch (true) {
+    case !!(sum === n):
+      return 'perfect';
+      break;
+
+    case !!(sum > n):
+      return 'abundant';
+      break;
+
+    default:
+      return 'deficient';
+  }
 };

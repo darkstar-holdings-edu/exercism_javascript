be95f1d8958b586a6e6fa82610693bb13b19ee0b
David Betz <hashref@gmail.com>
2023-03-29 17:42:08 -0700

    More updates suggested by ChatGPT4

--- anagram/anagram.js
+++ anagram/anagram.js
@@ -1,14 +1,26 @@
+/**
+ * Returns a character count object for a given word.
+ *
+ * @param {string} word - The word to get the character count object for.
+ * @returns {Object} The character count object for the given word.
+ */
 const getCharacterCounts = (word) => {
   const charCount = {};
   const lowercaseWord = word.toLowerCase();
-
   for (const char of lowercaseWord) {
     charCount[char] = (charCount[char] || 0) + 1;
   }
-
   return charCount;
 };
 
+/**
+ * Checks if two words are anagrams of each other.
+ *
+ * @param {string} word1 - The first word to check.
+ * @param {string} word2 - The second word to check.
+ * @param {Object} targetCharCount - The character count object for the target word.
+ * @returns {boolean} Whether the two words are anagrams of each other.
+ */
 const isAnagram = (word1, word2, targetCharCount) => {
   if (word1.length !== word2.length || word1.toLowerCase() === word2.toLowerCase()) {
     return false;
@@ -22,10 +34,17 @@ const isAnagram = (word1, word2, targetCharCount) => {
       return false;
     }
   }
-
   return true;
 };
 
+/**
+ * Returns an array of anagrams of a given target word from a list of candidates.
+ *
+ * @param {string} target - The target word to find anagrams of.
+ * @param {string[]} candidates - An array of candidate words to check for anagrams.
+ * @returns {string[]} An array of anagrams of the target word.
+ * @throws {Error} If the target parameter is not a string or the candidates parameter is not an array.
+ */
 export const findAnagrams = (target, candidates) => {
   if (typeof target !== 'string' || !Array.isArray(candidates)) {
     throw new Error('Invalid parameters');
@@ -39,6 +58,5 @@ export const findAnagrams = (target, candidates) => {
       anagrams.add(candidate);
     }
   }
-
   return Array.from(anagrams);
 };
--- isogram/isogram.js
+++ isogram/isogram.js
@@ -1,4 +1,9 @@
+/**
+ * Checks whether a given word is an isogram or not.
+ * @param {string} word - The word to check.
+ * @returns {boolean} Whether the word is an isogram or not.
+ */
 export const isIsogram = (word) => {
-  const chars = word?.toLowerCase()?.match(/[a-z]/g) ?? [];
-  return new Set([...chars]).size == chars.length;
+  const chars = word.toLowerCase().match(/[a-z]/g) ?? [];
+  return new Set([...chars]).size === chars.length;
 };

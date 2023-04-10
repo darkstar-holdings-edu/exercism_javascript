81824ef2b91cb07280f59e096625d3a6a7201143
David Betz <hashref@gmail.com>
2023-04-10 12:40:05 -0700

    Add solution for bob exercise

--- bob/bob.js
+++ bob/bob.js
@@ -1,8 +1,22 @@
-//
-// This is only a SKELETON file for the 'Bob' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export const hey = (message) => {
-  throw new Error('Remove this statement and implement this function');
+  if (message.trim().length === 0) {
+    return 'Fine. Be that way!';
+  }
+
+  const parsedMessage = message.replace(/[^a-zA-Z\?\!]/g, '');
+
+  switch (true) {
+    case parsedMessage.endsWith('?'):
+      if (/^[A-Z]+\?$/.test(parsedMessage)) {
+        return "Calm down, I know what I'm doing!";
+      }
+
+      return 'Sure.';
+
+    case /^[A-Z]+!*$/.test(parsedMessage):
+      return 'Whoa, chill out!';
+
+    default:
+      return 'Whatever.';
+  }
 };
